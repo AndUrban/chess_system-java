29a02e28bdceafa72cae6721856184a4ef4f4457
AndUrban <andreurbanwork@gmail.com>
2024-02-29 14:12:40 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,14 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
+		
+		Position position = new Position(8, 3);
+		
+		System.out.println(position);
 		
 	}
 
